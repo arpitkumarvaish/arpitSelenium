96b2322c398b67da0b917591a2206b734ed220f5
Arpit-Vaish <arpan.chakraborty@xpo.com>
2019-03-01 15:54:21 +0530

    FirstCommit

---
+++ arpit.automation.selenium/src/test/java/arpit/automation/selenium/DemoForMaven.java
@@ -0,0 +1,14 @@
+package arpit.automation.selenium;
+
+import org.testng.annotations.Test;
+
+public class DemoForMaven {
+
+	@Test
+	public void demo()
+	{
+		
+		System.out.println("Welcome to maven world");
+		
+	}
+}
